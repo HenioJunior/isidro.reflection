c18c92c0de884ac3939dbd38e81f468fda3abc10
Henio Junior <henioajunior@gmail.com>
2022-09-21 14:57:02 -0300

    Recuperando a Lista de Métodos

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Notas
 
+#### Reflection
+- Ler em tempo de execução uma determinada estrutura de objeto;
+- o `analyze` recebe qualquer Object e extrai a sua estrutura;
--- TesteReflection/src/Analyzer.java
+++ TesteReflection/src/Analyzer.java
@@ -1,8 +1,14 @@
+import java.lang.reflect.Method;
+
 public class Analyzer {
 
     public void analyze(Object o) {
         System.out.println("--> Recuperando a lista de métodos");
         try {
+            Method metodos[] = o.getClass().getDeclaredMethods();
+            for(Method m: metodos) {
+                System.out.println(m.getName() + ":" + m.getReturnType());
+            }
 
         }catch(Exception ex) {
             System.err.println("Erro ao recuperar a lista de métodos");
